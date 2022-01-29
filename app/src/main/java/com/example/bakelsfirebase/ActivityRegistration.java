e03bf38bafe96036b1dec4dbb543fc75a4d07c2e
lpilas24 <leiva20152@gmail.com>
2022-01-28 20:47:27 -0500

    Added registration clases and layouts of login and register.

---
+++ app/src/main/java/com/example/bakelsfirebase/ActivityRegistration.java
@@ -0,0 +1,18 @@
+package com.example.bakelsfirebase;
+
+import android.os.Bundle;
+import android.os.PersistableBundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.bakelsfirebase.R;
+
+public class ActivityRegistration extends AppCompatActivity {
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registration);
+    }
+}
